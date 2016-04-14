bf030df0818529560590825aa73c68eee1cf616e
Som Shankar Bhattacharyya <shankarb@lexmark.com>
2016-04-14 17:36:27 +0530

    Updated the Cake Pattern Dependency Injection example with comments

--- src/main/scala/DI.scala
+++ src/main/scala/DI.scala
@@ -1,23 +1,23 @@
-package examples
+//All future versions of DAO will extend this
 trait AbstractDAO{
   def getRecords:String
   def updateRecords(records:String):Unit
 }
-
+//One concrete version
 trait concreteDAO extends AbstractDAO{
   override def getRecords={"Here are records"}
   override def updateRecords(records:String){
     println("Updated "+records)
   }
 }
-
+//One concrete version
 trait concreteDAO1 extends AbstractDAO{
   override def getRecords={"Records returned from DAO2"}
   override def updateRecords(records:String){
     println("Updated via DAO2"+records)
   }
 }
-
+//This trait just defines dependencies (in this case an instance of AbstractDAO) and defines operations based over that
 trait service{
   this:AbstractDAO =>
     
@@ -31,11 +31,12 @@ trait service{
 
 
 object DI extends App{
-  val wiredObject = new service with concreteDAO //Wiring up the DAOs to the service here
+  val wiredObject = new service with concreteDAO //injecting concrete DAO to the service and calling methods
   wiredObject.updateRecords("RECORD1")
   println(wiredObject.getRecords)
   
-  val wiredObject1 = new service with concreteDAO1//Wiring
+  val wiredObject1 = new service with concreteDAO1
   wiredObject1.updateRecords("RECORD2")
   println(wiredObject1.getRecords)
+  
 }
