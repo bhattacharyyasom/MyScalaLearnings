02f2188396861e13eb5967dbb332db7469ff7d87
Som Shankar Bhattacharyya <shankarb@lexmark.com>
2017-08-17 19:34:58 +0530

    Added documentation to explain the DI using Reader

--- src/main/scala/ReaderMonadDI.scala
+++ src/main/scala/ReaderMonadDI.scala
@@ -1,6 +1,15 @@
 import scalaz._
 import Scalaz._
 
+/**
+ * Here we describe two ways of wiring up beans using dependency injection using Reader monad.
+ *  1.The AbstractDAO trait is extended by concrete DAOs.
+ *  2.The Service trait provides methods that return Readers of methods that perform some action "given" the dependency.
+ *  3.MyTestService extends the Service trait and provides methods to operate on the Readers returned from the DAO. So in effect the Service trait in this case
+ *    acts as the "bridge" if you will
+ *  4.Lastly we show two ways of getting the dependency injected from the outside ie. the stub
+ */
+
 object ReaderMonadDITest extends App {
   trait AbstractDAO {
     def getRecords: String
@@ -41,10 +50,10 @@ object ReaderMonadDITest extends App {
 
   //Stub to test out DI
   val serviceObject = new myTestService
-  serviceObject.updateRecordsViaService("RECORD1")(new concreteDAO {}) //Provide the dep. here and execute
+  serviceObject.updateRecordsViaService("RECORD1").run(new concreteDAO {}) //Provide the dep. here and execute
   println(serviceObject.getRecordsViaService(new concreteDAO {})) //We provide that function the dep. here and execute it
 
-  val serviceObject1 = new Service {}
-  serviceObject1.updateRecordsViaDAO("RECORD2")(new concreteDAO1 {})
+  val serviceObject1 = new Service {} //Instantiate a new instance of the Service trait and use directly
+  serviceObject1.updateRecordsViaDAO("RECORD2")(new concreteDAO1 {}) //Inject the concreteDao here.
   println(serviceObject1.getRecordsViaDAO(new concreteDAO1 {}))
 }
